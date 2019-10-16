72686c2c9d48876472689750d16ddee15c09a253
Andres <Andres@grafxnwmbp.local>
2019-10-16 14:33:15 -0700

    adding useContext, context cart/product, add/deleteItem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './contexts/ProductContext.js';
+import { CartContext } from './contexts/CartContext.js';
 
 // Components
 import Navigation from './components/Navigation';
@@ -13,28 +15,33 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
+	const deleteItem = itemid => {
+		const newCart = cart.filter(
+		  item => item.id !== itemid
+		);
+		return setCart([...newCart]);
+	  };
+
 	return (
+
 		<div className="App">
-			<Navigation cart={cart} />
+		 <ProductContext.Provider value={{ products, addItem }}>
+		 	<CartContext.Provider value ={{cart, deleteItem}}>
+			 <Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+				{/* Routes */}
+				<Route exact path="/" component={Products}/>
+				<Route
+					path="/cart"
+					render={() => <ShoppingCart cart={cart} />}
+				/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+			 </CartContext.Provider>
+		 </ProductContext.Provider>
+			
 		</div>
 	);
 }
